0e46a75af9d6fc1a19564caaa48702a33ca9ad36
Raquel Domingo <raqueldomingo@Raquel-Domingo.local>
2017-10-22 15:23:42 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  colors: string[];
+  barcode: string[];
+
+  constructor() {
+    this.title = 'Retro Barcode Generator';
+    this.colors = ['MediumAquaMarine', 'MediumBlue', 'DarkCyan', 'AquaMarine', 'CadetBlue', 'CornflowerBlue', 'DarkBlue', 'DarkGreen', 'DarkSeaGreen', 'DarkSlateGray', 'ForestGreen', 'LightSeaGreen', 'LimeGreen', 'MediumSeaGreen', 'MediumSpringGreen', 'SeaGreen'];
+    this.barcode = [];
+    for (let x = 0; x < 10; x++){
+      this.barcode[x] = this.colors[Math.floor(Math.random()*this.colors.length)];
+    }
+  }
 }
